5158c501028cdfe21ceca1643222fffc466bbbd7
Sereyvuth CHUM <sereyvuth.chum@hetic.net>
2018-12-14 11:47:29 +0100

    Add files via upload

---
+++ valid_form.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include "header.php" ?>
+  </head>
+  <body>
+
+  <?php include "topbar.php" ?>
+    
+
+    <section class="ftco-section-parallax test-section">
+      <div class="parallax-img d-flex align-items-center">
+        <div class="container">
+          <div class="row d-flex justify-content-center">
+            <div class="col-md-7 text-center heading-section heading-section-white ftco-animate">
+              <h2>Merci d'avoir répondu au formulaire.</h2>
+              <div class="row d-flex justify-content-center mt-5">
+                <div class="col-md-8">
+                  <a class="test-link" href="index.php"><span>Revenir à l'accueil</span></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <?php include "footer.php" ?>
+
+
+  <?php include "js-import.php" ?>
+
+  </body>
+</html>
