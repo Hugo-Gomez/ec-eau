556f66930e9775eb0def4c82bcab27849f694bb3
BOCQUILLON pierre <pierre.bocquillon@hetic.net>
2018-12-14 12:20:54 +0100

    V1.0

--- functions/addResult.php
+++ functions/addResult.php
@@ -110,44 +110,43 @@ function addResult($conn,$mail,$age,$sexe,$ville,$cp,$freqDouche,$tempsDouche,$d
 $res = getResult($conn);
 for($i = 0; $i <= $res.count(); $i++){
     if($res[i]['mail'] == $_POST['email']){
-        
-addResult(
-    $conn,
-    $_POST['email'],
-    $_POST['age'],
-    $_POST['sexe'],
-    $_POST['ville'],
-    $_POST['cp'],
-    $_POST['q1a'],
-    $_POST['q1b'],
-    $_POST['q1c'],
-    $_POST['q2a'],
-    $_POST['q2b'],
-    $_POST['q3a'],
-    $_POST['q3b'],
-    $_POST['q4a'],
-    $_POST['q4b'],
-    $_POST['q4c'],
-    $_POST['q4d'],
-    $_POST['q5a'],
-    $_POST['q5b'],
-    $_POST['q6b'],
-    $_POST['q7a'],
-    $_POST['q7b'],
-    $_POST['q7c'],
-    $_POST['q7d'],
-    $_POST['q8a'],
-    $_POST['q8b'],
-    $_POST['q8c'],
-    $_POST['q9a'],
-    $_POST['q9b'],
-    $_POST['q9c'],
-    $_POST['q10'],
-    $_POST['q11']
-);
-
-header('location:/valid_form.php');
+                
     }else{
-        header('location:/already_form.php');
+        addResult(
+            $conn,
+            $_POST['email'],
+            $_POST['age'],
+            $_POST['sexe'],
+            $_POST['ville'],
+            $_POST['cp'],
+            $_POST['q1a'],
+            $_POST['q1b'],
+            $_POST['q1c'],
+            $_POST['q2a'],
+            $_POST['q2b'],
+            $_POST['q3a'],
+            $_POST['q3b'],
+            $_POST['q4a'],
+            $_POST['q4b'],
+            $_POST['q4c'],
+            $_POST['q4d'],
+            $_POST['q5a'],
+            $_POST['q5b'],
+            $_POST['q6b'],
+            $_POST['q7a'],
+            $_POST['q7b'],
+            $_POST['q7c'],
+            $_POST['q7d'],
+            $_POST['q8a'],
+            $_POST['q8b'],
+            $_POST['q8c'],
+            $_POST['q9a'],
+            $_POST['q9b'],
+            $_POST['q9c'],
+            $_POST['q10'],
+            $_POST['q11']
+        );
+        header('location:/valid_form.php');
     }
+    header('location:/already_form.php');
 }
