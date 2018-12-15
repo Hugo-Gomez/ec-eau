a3c5f2fdda3ebe3d70b1f752ef3a29f07be1ae83
BOCQUILLON pierre <pierre.bocquillon@hetic.net>
2018-12-15 18:05:20 +0100

    V1.0

--- functions/addResult.php
+++ functions/addResult.php
@@ -1,7 +1,7 @@
 <?php
 require_once './connection.php';
 require_once './getResult.php';
-function addResult($conn,$mail,$age,$sexe,$ville,$cp,$freqDouche,$tempsDouche,$debitDouche,$freqBain,$rempBain,$freqDents,$eauDents,$choixVaisselle,$freqVaisselle,$methVaisselle,$couvVaisselle,$freqMal,$dateMal,$ecoeau,$plante,$momentPlante,$eauPlante,$freqPlante,$voiture,$methVoiture,$freqVoiture,$piscine,$freqPiscine,$volumePiscine,$etiquette,$bouteille){
+function addResult($conn,$mail,$age,$sexe,$ville,$cp,$freqDouche,$tempsDouche,$debitDouche,$freqBain,$rempBain,$freqDents,$eauDents,$freqVaisselle,$choixVaisselle,$methVaisselle,$couvVaisselle,$freqMal,$dateMal,$ecoeau,$plante,$momentPlante,$eauPlante,$freqPlante,$voiture,$methVoiture,$freqVoiture,$piscine,$freqPiscine,$volumePiscine,$etiquette,$bouteille){
     $sql="INSERT INTO 
     `result`
     (
@@ -107,44 +107,59 @@ function addResult($conn,$mail,$age,$sexe,$ville,$cp,$freqDouche,$tempsDouche,$d
     errorHandler($stmt);
 }
 
-$res = getResult($conn);
-for($i = 0; $i <= $res.count(); $i++){
-    if($res[i]['mail'] == $_POST['email']){
-        header('location:/already_form.php');
+$sql="SELECT 
+        `mail`
+    FROM 
+        `result`
+    WHERE
+        `mail` = :mail
+    ;";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindValue(':mail', $_POST['email']);
+    $stmt->execute();
+    errorHandler($stmt);
+    $mail=null;
+    while(false !== $row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $mail[]=$row;
     }
+
+if(isset($mail[0])){
+    header('location:/already_form.php');
+}else{
+
+addResult(
+    $conn,
+    $_POST['email'],
+    $_POST['age'],
+    $_POST['sexe'],
+    $_POST['ville'],
+    $_POST['cp'],
+    $_POST['q1a'],
+    $_POST['q1b'],
+    $_POST['q1c'],
+    $_POST['q2a'],
+    $_POST['q2b'],
+    $_POST['q3a'],
+    $_POST['q3b'],
+    $_POST['q4a'],
+    $_POST['q4b'],
+    $_POST['q4c'],
+    $_POST['q4d'],
+    $_POST['q5a'],
+    $_POST['q5b'],
+    $_POST['q6b'],
+    $_POST['q7a'],
+    $_POST['q7b'],
+    $_POST['q7c'],
+    $_POST['q7d'],
+    $_POST['q8a'],
+    $_POST['q8b'],
+    $_POST['q8c'],
+    $_POST['q9a'],
+    $_POST['q9b'],
+    $_POST['q9c'],
+    $_POST['q10'],
+    $_POST['q11']
+);
+header('location:/valid_form.php');
 }
-    addResult(
-        $conn,
-        $_POST['email'],
-        $_POST['age'],
-        $_POST['sexe'],
-        $_POST['ville'],
-        $_POST['cp'],
-        $_POST['q1a'],
-        $_POST['q1b'],
-        $_POST['q1c'],
-        $_POST['q2a'],
-        $_POST['q2b'],
-        $_POST['q3a'],
-        $_POST['q3b'],
-        $_POST['q4a'],
-        $_POST['q4b'],
-        $_POST['q4c'],
-        $_POST['q4d'],
-        $_POST['q5a'],
-        $_POST['q5b'],
-        $_POST['q6b'],
-        $_POST['q7a'],
-        $_POST['q7b'],
-        $_POST['q7c'],
-        $_POST['q7d'],
-        $_POST['q8a'],
-        $_POST['q8b'],
-        $_POST['q8c'],
-        $_POST['q9a'],
-        $_POST['q9b'],
-        $_POST['q9c'],
-        $_POST['q10'],
-        $_POST['q11']
-    );
-    header('location:/valid_form.php');
