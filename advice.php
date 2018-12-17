5b0b860b799a0fc5c13e1a3f3cef75a4616c3115
Sereyvuth CHUM <sereyvuth.chum@hetic.net>
2018-12-17 12:09:34 +0100

    Add files via upload

--- advice.php
+++ advice.php
@@ -87,19 +87,21 @@
         <div class="row no-gutters">
           <div class="col-md-4 ftco-animate py-5 nav-link-wrap">
             <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-              <a class="nav-link px-4 active" id="v-pills-master-tab" data-toggle="pill" href="#v-pills-master" role="tab" aria-controls="v-pills-master" aria-selected="true"><i class="fas fa-shower"></i> Douche</a>
+              <a class="nav-link px-4 active" id="v-pills-master-tab" data-toggle="pill" href="#v-pills-master" role="tab" aria-controls="v-pills-master" aria-selected="true"><i class="fas fa-shower"></i> Bain et Douche</a>
 
-              <a class="nav-link px-4" id="v-pills-tooth-tab" data-toggle="pill" href="#v-pills-tooth" role="tab" aria-controls="v-pills-tooth" aria-selected="false"><i class="fas fa-tooth"></i> Dents</a>
+              <a class="nav-link px-4" id="v-pills-tooth-tab" data-toggle="pill" href="#v-pills-tooth" role="tab" aria-controls="v-pills-tooth" aria-selected="false"><i class="fas fa-tooth"></i> Brossage de dents</a>
 
               <a class="nav-link px-4" id="v-pills-dish-tab" data-toggle="pill" href="#v-pills-dish" role="tab" aria-controls="v-pills-dish" aria-selected="false"><i class="fas fa-utensils"></i> Vaisselle</a>
 
-              <a class="nav-link px-4" id="v-pills-clothes-tab" data-toggle="pill" href="#v-pills-clothes" role="tab" aria-controls="v-pills-clothes" aria-selected="false"><i class="fas fa-socks"></i> Machine à laver</a>
+              <a class="nav-link px-4" id="v-pills-clothes-tab" data-toggle="pill" href="#v-pills-clothes" role="tab" aria-controls="v-pills-clothes" aria-selected="false"><i class="fas fa-socks"></i> Linge</a>
 
               <a class="nav-link px-4" id="v-pills-garden-tab" data-toggle="pill" href="#v-pills-garden" role="tab" aria-controls="v-pills-garden" aria-selected="false"><i class="fab fa-pagelines"></i> Jardin</a>
 
               <a class="nav-link px-4" id="v-pills-car-tab" data-toggle="pill" href="#v-pills-car" role="tab" aria-controls="v-pills-car" aria-selected="false"><i class="fas fa-car"></i> Voiture</a>
 
               <a class="nav-link px-4" id="v-pills-drink-tab" data-toggle="pill" href="#v-pills-drink" role="tab" aria-controls="v-pills-drink" aria-selected="false"><i class="fas fa-tint"></i> Eau à boire</a>
+             
+              <a class="nav-link px-4" id="v-pills-swimmer-tab" data-toggle="pill" href="#v-pills-swimmer" role="tab" aria-controls="v-pills-swimmer" aria-selected="false"><i class="fas fa-swimmer"></i> Piscine</a>
             </div>
           </div>
           <div class="col-md-8 ftco-animate p-4 p-md-5 d-flex align-items-center">
@@ -108,52 +110,81 @@
 
               <div class="tab-pane fade show active py-5" id="v-pills-master" role="tabpanel" aria-labelledby="v-pills-master-tab">
                 <i class="icon mb-3 d-block fas fa-shower"></i>
-                <h2 class="mb-4">Sous la douche</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-                <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
+                <h2 class="mb-4">Douche et bain</h2>
+                <p>Un bain consomme plus de 100 litres d’eau. Une douche est plus économique, sauf si on traîne un peu sous l'eau mais 10 minutes de douche peuvent consommer plus d’eau qu’un bain.
+</p>
+<p>On peut penser à :</p>
+<ul>
+  <li>Recycler l’eau de la douche</li>
+  <li>Utiliser des pommeaux de douche munis du label 'energy' ( moins de 12 L par minute), des réducteurs de débits, stop-douche...</li>
+</ul>
               </div>
 
               <div class="tab-pane fade py-5" id="v-pills-tooth" role="tabpanel" aria-labelledby="v-pills-tooth-tab">
                 <i class="icon mb-3 d-block fas fa-tooth"></i>
                 <h2 class="mb-4">En se lavant les dents</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-                <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
-              </div>
+                <p>Il est préférable de se rincer avec un verre à dents (10 000 litres d'eau gaspillés par an) ou bien de couper son robinet en se brossant les dents fait également partie des écogestes basiques à mettre en place chez soi.
+</p>
+               </div>
 
               <div class="tab-pane fade py-5" id="v-pills-dish" role="tabpanel" aria-labelledby="v-pills-dish-tab">
                 <i class="icon mb-3 d-block fas fa-utensils"></i>
                 <h2 class="mb-4">En faisant la vaisselle</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-                <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
-              </div>
+                <p>Si on fait la vaisselle à la main, il est plus économe de remplir les deux bacs de l’évier (laisser couler l'eau consomme jusqu'à 200 litres par vaisselle).
+</p>
+<p>Pour une quantité assez importante de vaisselle, l’utilisation d’un lave vaisselle consomme donc en général moins d’eau qu’un lavage à la main. Si la vaisselle ne remplit pas la machine, il est préférable de la laver à la main.
+</p>
+<p>Si vous optez pour un lave-vaisselle, regardez bien les étiquettes énergétiques qui indiquent la consommation d’eau et achetez des appareils supérieur à la classe A.</p>
+                 </div>
 
               <div class="tab-pane fade py-5" id="v-pills-clothes" role="tabpanel" aria-labelledby="v-pills-clothes-tab">
                 <i class="icon mb-3 d-block fas fa-socks"></i>
                 <h2 class="mb-4">Machine à laver</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-                <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
-              </div>
+                <p>Sur les machines qui ne disposent pas la technologie de la reconnaissance du poids du linge, veillez à remplir suffisamment de linge avant de lancer le cycle. Profitez également de la fonction cycle court, ou demi-charge, si votre lave-linge le permet.</p>
+                </div>
 
               <div class="tab-pane fade py-5" id="v-pills-garden" role="tabpanel" aria-labelledby="v-pills-garden-tab">
                 <i class="icon mb-3 d-block fab fa-pagelines"></i>
                 <h2 class="mb-4">S'occuper de son jardin</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-                <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
-              </div>
+                <ul>
+  <li>Remplacer le tuyau d'arrosage par un arrosoir ;
+</li>
+  <li>Arroser moins souvent mais plus longtemps pour que la terre s'humidifie en profondeur. L'arrosage rotatif par jets alternatifs permet un arrosage « doux » sur une longue période.
+</li>
+<li>Veiller à ce que le sol soit assez meuble et travaillé. Cela permet un arrosage plus efficace. En effet, l'eau ruisselle sur un sol tassé ;
+</li>
+<li>Au potager ou dans les plates-bandes, recouvrir le sol entre les plantes de paille, tontes de pelouse, feuilles, etc. favorise la rétention de l'eau ;
+</li>
+<li>Arroser le soir, pour éviter les gaspillages dus à l'évaporation (il fait plus chaud en journée) et l'évapotranspiration (rejet de vapeur par les plantes).
+</li>
+</ul>
+                </div>
 
               <div class="tab-pane fade py-5" id="v-pills-car" role="tabpanel" aria-labelledby="v-pills-car-tab">
                 <i class="icon mb-3 d-block fas fa-car"></i>
                 <h2 class="mb-4">Laver sa voiture</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-                <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
-              </div>
+                <p>Laver la voiture en station et non chez-soi. En plus d’être souillée par les graisses et hydrocarbures, l’eau utilisée est de 3 fois supérieur à celle utilisée dans une station de nettoyage haute pression.</p>
+                </div>
 
-              <div class="tab-pane fade py-5" id="v-pills-drink" role="tabpanel" aria-labelledby="v-pills-drink-tab">
+                <div class="tab-pane fade py-5" id="v-pills-drink" role="tabpanel" aria-labelledby="v-pills-drink-tab">
                 <i class="icon mb-3 d-block fas fa-tint"></i>
                 <h2 class="mb-4">Eau à boire</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
-                <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
+                <p>Selon 
+                <a target="_blank" style ="text-decoration: underline;" href="https://www.quechoisir.org/guide-d-achat-quelle-eau-boire-n4855/">UFC-Que Choisir</a>, si vous habitez dans les moyennes et grandes villes, il vaut mieux privilégier l’eau du robinet, plus écologique et moins chère que celle en bouteille[19]. La qualité de l’eau potable de sa commune peut être vérifier grâce à la carte du ministère des Solidarités et de la Santé en cliquant sur ce <a target="_blank" style ="text-decoration: underline;" href="https://solidarites-sante.gouv.fr/sante-et-environnement/eaux/article/qualite-de-l-eau-potable">lien.</a></p>
+                <p>
+                <a target="_blank" style ="text-decoration: underline;" href="https://www.linfodurable.fr/conso/eau-en-bouteille-eau-du-robinet-laquelle-choisir-2834
+">L’UFC-Que Choisir</a> recommande aux personnes souhaitant tout de même en utiliser de remplacer régulièrement la cartouche, de conserver la carafe filtrante et son eau en réfrigérateur et de consommer l’eau filtrée rapidement, idéalement dans les 24 heures après filtration.</p>
               </div>
+
+              <div class="tab-pane fade py-5" id="v-pills-swimmer" role="tabpanel" aria-labelledby="v-pills-drink-tab">
+                <i class="icon mb-3 d-block fas fa-swimmer"></i>
+                <h2 class="mb-4">Piscine</h2>
+                <ul>
+                <li>
+                Bâcher la piscine pour limiter l’évaporation.</li>
+                <li>Changer ¼ du volume d’eau annuellement. Pour plus d'informations, <a target="_blank" style ="text-decoration: underline;" href="https://www.sa-piscine.com/cout-annuel-eau">cliquez ici.</a></li>
+                </ul>
+                </div>
             </div>
           </div>
         </div>
